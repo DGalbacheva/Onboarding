e8060d7399e20049307cf9e6435a217810e8fb49
DGalbacheva <dglbacheva@gmail.com>
2024-11-18 14:26:04 +0300

    Added DataSource and page swipe support.

--- Onboarding/OnboardingViewController.swift
+++ Onboarding/OnboardingViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-final class OnboardingViewController: UIPageViewController {
+final class OnboardingViewController: UIPageViewController, UIPageViewControllerDataSource {
     lazy var pages: [UIViewController] = {
         let red = UIViewController()
         red.view.backgroundColor = .red
@@ -25,8 +25,43 @@ final class OnboardingViewController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // install OnboardingViewController with its own DataSource
+        dataSource = self
+        
         if let first = pages.first {
             setViewControllers([first], direction: .forward, animated: true, completion: nil)
         }
+    }
+    
+    // MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        // return the previous (relative to the passed viewController) child controller
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        // return the next (relative to the passed viewController) child controller
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else {
+            return nil
+        }
+        
+        return pages[nextIndex]
     }
 }
